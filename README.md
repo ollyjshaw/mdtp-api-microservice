5d74027a5fac3d3630020cd0cdc80901b4956d29
petrnejedly <pnejedly@equalexperts.com>
2015-09-08 14:09:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+mdtp-api-microservice
+=====================
+
+This is a Typesafe Activator template for HMRC API microservices.
+
+To create a new API microservice, use this command:
+
+```
+activator new PROJECTNAME mdtp-api-microservice
+```
---
+++ project/MicroServiceBuild.scala
@@ -0,0 +1,51 @@
+import sbt._
+import uk.gov.hmrc.SbtAutoBuildPlugin
+import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
+import uk.gov.hmrc.versioning.SbtGitVersioning
+
+object MicroServiceBuild extends Build with MicroService {
+
+  override val appName = "mdtp-api-microservice"
+
+  override lazy val plugins: Seq[Plugins] = Seq(
+    SbtAutoBuildPlugin, SbtGitVersioning, SbtDistributablesPlugin
+  )
+
+  override lazy val appDependencies: Seq[ModuleID] = AppDependencies()
+}
+
+private object AppDependencies {
+  import play.core.PlayVersion
+
+  private val microserviceBootstrapVersion = "1.4.0"
+  private val playHealthVersion = "1.1.0"
+  private val playConfigVersion = "1.2.0"
+  private val hmrcTestVersion = "1.2.0"
+  private val playReactivemongoVersion = "4.1.0"
+  private val simpleReactivemongoVersion = "3.1.2"
+
+  val compile = Seq(
+    "uk.gov.hmrc" %% "play-reactivemongo" % playReactivemongoVersion,
+    "uk.gov.hmrc" %% "simple-reactivemongo" % simpleReactivemongoVersion,
+    "uk.gov.hmrc" %% "microservice-bootstrap" % microserviceBootstrapVersion,
+    "uk.gov.hmrc" %% "play-health" % playHealthVersion,
+    "uk.gov.hmrc" %% "play-config" % playConfigVersion,
+    "uk.gov.hmrc" %% "play-json-logger" % "1.0.0"
+  )
+
+  abstract class TestDependencies(scope: String) {
+    lazy val test : Seq[ModuleID] = Seq(
+      "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
+      "org.scalatest" %% "scalatest" % "2.2.2" % scope,
+      "org.pegdown" % "pegdown" % "1.4.2" % scope,
+      "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+    )
+  }
+
+  object Test extends TestDependencies("test")
+  object IntegrationTest extends TestDependencies("it")
+
+  def apply() = compile ++ Test.test ++ IntegrationTest.test
+
+}
+
